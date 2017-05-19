687c68ccafa09360400ab607fd35e81547f742a0
martin-pabst <asv.pabst@gmail.com>
2017-05-19 15:53:37 +0200

    Event form: Tab absences (only GUI)

--- WebContent/test/eventdetailform/eventdetails.js
+++ WebContent/test/eventdetailform/eventdetails.js
@@ -34,14 +34,72 @@ $(function(){
             $('#eventDateTimeFrom').data("DateTimePicker").maxDate(e.date);
         });
 
+        loadValues();
 
     });
 
-    $('#calendarDetailsForm').validator().on('submit', function(e){
+    $('#eventAbsenceTab').on('shown.bs.tab', function () {
+
+        // Alle Klassen anwesend
+        // $('#eventAbsence').find('label').removeClass('active');
+
+        $('#eventAbsence').find('label').on('focusin', (function(e){
+
+            e.stopImmediatePropagation();
+
+        }));
+
+        $('#eventAbsence').find('button').click(function(e){
+
+            var labels = $(this).siblings().find('label');
+
+            var isSelected = $(labels[0]).hasClass('active');
+
+            if(isSelected){
+                labels.removeClass('active');
+            } else {
+                labels.addClass('active');
+            }
+
+
+        });
+
+        $('#eventAbsencesWholeSchool').change(function(){
+            if($(this).is(':checked')){
+                $('#eventAbsencesMatrix').hide();
+            } else {
+                $('#eventAbsencesMatrix').show();
+            }
+        });
+
+    });
+
+        $('#calendarDetailsForm').validator().on('submit', function(e){
         if(!e.isDefaultPrevented()){
             alert('hier!');
         }
 
     });
 
+
+
 });
+
+function loadValues(){
+
+    $('#eventName').val('Name des Events');
+    $('#eventnameshort').val('Shortname');
+    $('#eventDateTimeFrom').data('DateTimePicker').date('17.05.2017');
+    $('#eventDateTimeTo').data('DateTimePicker').date('18.05.2017 10:02');
+    $('#eventWholeDay').prop('checked', false);
+    $('#eventLocation').val('Ingolstadt');
+    $('#eventDescription').val('Beschreibung...\nNoch eine Zeile');
+
+    $('#eventRestriction').find('option[value="3"]').prop('selected', true);
+
+    $('#eventAbsencesWholeSchool').prop('checked', false);
+    $('#eventAbsencesMatrix').find('label[text="5a"]').addClass('active');
+    $('#eventAbsencesNoBigTests').prop('checked', false);
+    $('#eventAbsencesNoBigTests').prop('checked', true);
+
+}
